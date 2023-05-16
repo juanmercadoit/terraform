26d6444faad035434edb88fe8bb20fb8110516dc
Juan Andres Mercado <andres.mercado@hotmail.com>
2023-05-16 19:42:47 -0300

    Add Kubernetes provider for terraform and AWS.

---
+++ terraform-eks/k8s.tf
@@ -0,0 +1,6 @@
+# Kubernetes provider for terraform and AWS
+provider "kubernetes" {
+    host = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token = data.aws_eks_cluster_auth.cluster.token
+}
